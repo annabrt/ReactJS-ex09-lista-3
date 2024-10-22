04a81c83b6c198d5cc199b25c2cffbd4ea0266ce
Anna Bertochi <anafbrtc@gmail.com>
2024-10-21 22:58:54 -0300

    Add files via upload

---
+++ ex09l3/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ListaItens from './components/ListaItens';
+
+function App() {
+  return (
+    <div>
+      <ListaItens />
+    </div>
+  );
+}
+
+export default App;
